fc9dbee6513f93af09a6e434c7a80cc4b27b3e42
BruegelN <BruegelN@users.noreply.github.com>
2016-03-21 21:27:23 +0100

    added comments to TestingDemo

--- app/src/main/java/de/dhbw/kassenautomat/TestingDemo.java
+++ app/src/main/java/de/dhbw/kassenautomat/TestingDemo.java
@@ -2,14 +2,25 @@ package de.dhbw.kassenautomat;
 
 /**
  * Created by nicob on 21.03.16.
+ *
+ * Just to show the possibilities of unit testing with JUnit.
  */
 public class TestingDemo {
 
+    /**
+     * Will return a completely random number!
+     * @return 4
+     */
     public int getRandomNumber()
     {
         return 4; // thanks to xkcd for the advice
     }
 
+    /**
+     * Just to test exception handling as well.
+     *
+     * @throws Exception
+     */
     public void throwAnEvilExcepttion() throws Exception
     {
         int truth = 42;
