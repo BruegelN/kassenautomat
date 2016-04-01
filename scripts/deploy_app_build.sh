1b257a4a0eb87a948d0ad051aaad3dc7e88813ea
BruegelN <BruegelN@users.noreply.github.com>
2016-04-01 16:48:30 +0200

    Fixed pull request check and hopefully really fixed output path....

--- scripts/deploy_app_build.sh
+++ scripts/deploy_app_build.sh
@@ -2,13 +2,14 @@
 set -ev
 
 ## prepare output path coresponding to build number and branch
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  # we're on a pull request thus append '-PR' to output path
-  OUTPUT_PATH="${OUTPUT_PATH}-PR"
 OUTPUT_PATH="~/public-html/kassenautomat/${TRAVIS_BUILD_NUMBER}_${TRAVIS_BRANCH}"
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+  # we're building a pull request
+  echo "Don't deploy pull requests :)"
+  exit 0
 fi
 
 echo "Output path: ${OUTPUT_PATH}"
 ## upload to ssh page
 export SSHPASS=$SSH_PASSWD
-sshpass -e scp -r ./app/build $SSH_USER@$SSH_DOMAIN:$OUTPUT_PATH
+sshpass -e scp -r ./app/build $SSH_USER@$SSH_DOMAIN:${OUTPUT_PATH}
