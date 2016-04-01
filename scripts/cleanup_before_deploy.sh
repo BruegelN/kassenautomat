f81b6fab52bc207385cc4bafe38f2aa218a120d2
BruegelN <BruegelN@users.noreply.github.com>
2016-04-01 15:17:20 +0200

    Print every commands before execution and run cleanup script before deploy.

--- scripts/cleanup_before_deploy.sh
+++ scripts/cleanup_before_deploy.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
+set -ev
 
 APP_BUILD_PATH="./app/build"
 APP_LINTER_PATH="${APP_BUILD_PATH}/linter"
 ## remove auto generated things
-echo "rm -rf ${APP_BUILD_PATH}/generated"
 rm -rf $APP_BUILD_PATH/generated
 
 ## remove intermediates
-echo "rm -rf ${APP_BUILD_PATH}/tmp"
 rm -rf $APP_BUILD_PATH/intermediates
 
 ## remove temp
-echo "rm -rf $APP_BUILD_PATH/tmp"
 rm -rf $APP_BUILD_PATH/tmp
 
 ## remove XML test-results
-echo "rm -rf ${APP_BUILD_PATH}/test-results"
 rm -rf $APP_BUILD_PATH/test-results
 
 ## Move apps directily into build-dir
-echo "Move APKs"
 mv $APP_BUILD_PATH/outputs/apk $APP_BUILD_PATH/apk
 
 ## Reorder linter results
-echo "Move linter results"
 
 # first "lint-results-release-fatal" related stuff
 mkdir -p $APP_LINTER_PATH/lint-results-release-fatal/lint-results-release-fatal_files
@@ -36,5 +31,4 @@ mv $APP_BUILD_PATH/outputs/lint-results.html $APP_LINTER_PATH/lint-results/index
 mv $APP_BUILD_PATH/outputs/lint-results_files $APP_LINTER_PATH/lint-results/
 
 ## remove output folder because we allread moved the important things elsewhere
-echo "rm -rf ${APP_BUILD_PATH}/outputs"
 rm -rf $APP_BUILD_PATH/outputs
