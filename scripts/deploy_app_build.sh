8324e93a1ded1a2db2b7d318d74a453373fd2ab2
BruegelN <BruegelN@users.noreply.github.com>
2016-04-01 16:25:26 +0200

    Hopefully fixed output-path for deployment-server.

--- scripts/deploy_app_build.sh
+++ scripts/deploy_app_build.sh
@@ -2,10 +2,10 @@
 set -ev
 
 ## prepare output path coresponding to build number and branch
-OUTPUT_PATH="${SSH_BASE_PATH}/${TRAVIS_BUILD_NUMBER}-${TRAVIS_BRANCH}"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
   # we're on a pull request thus append '-PR' to output path
   OUTPUT_PATH="${OUTPUT_PATH}-PR"
+OUTPUT_PATH="~/public-html/kassenautomat/${TRAVIS_BUILD_NUMBER}_${TRAVIS_BRANCH}"
 fi
 
 echo "Output path: ${OUTPUT_PATH}"
