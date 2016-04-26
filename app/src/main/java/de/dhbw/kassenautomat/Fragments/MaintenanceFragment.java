b8e56501cb6b65b029edcc4e1fcbbed6f64b7f9b
BruegelN <Nico.Bruegel@gmx.de>
2016-04-26 10:04:36 +0200

    Added working buttons for MaintenanceFragment.
    Buttons are connected to view elements by id.
    OverviewFragment element is created and switch to it when needed.

--- app/src/main/java/de/dhbw/kassenautomat/Fragments/MaintenanceFragment.java
+++ app/src/main/java/de/dhbw/kassenautomat/Fragments/MaintenanceFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 // To access the XML layouts easily
 import de.dhbw.kassenautomat.R;
@@ -15,9 +17,21 @@ import de.dhbw.kassenautomat.R;
  */
 public class MaintenanceFragment extends Fragment {
 
+    private Button btnResetCoins;
+    private Button btnEndMaintenance;
+
+    private OverviewFragment FragmentOverview;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        /**
+         * Instantiate fragment to return to overview.
+         */
+        FragmentOverview = (OverviewFragment) Fragment.instantiate(this.getActivity(), OverviewFragment.class.getName(), null);
+
+
         /* TODO initialize fields before createView
+           TODO read coin level from DB an set coresponding view elements
             and add elements to list form database
          */
         super.onCreate(savedInstanceState);
@@ -32,6 +46,12 @@ public class MaintenanceFragment extends Fragment {
         */
         View layoutMaintenance = inflater.inflate(R.layout.fragement_maintenance, null);
 
+        btnResetCoins = (Button) layoutMaintenance.findViewById(R.id.btnResetCoins);
+        btnEndMaintenance = (Button) layoutMaintenance.findViewById(R.id.btnEndMaintenance);
+
+        btnResetCoins.setOnClickListener(btnResetCoinsPressed);
+        btnEndMaintenance.setOnClickListener(btnEndMaintenancePressed);
+
         // so it can be displayed
         return layoutMaintenance;
     }
@@ -41,4 +61,22 @@ public class MaintenanceFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
     }
+
+    private View.OnClickListener btnResetCoinsPressed = new View.OnClickListener() {
+        public void onClick(View v) {
+
+            // TODO reset coin level!
+            Toast.makeText(getActivity(), "TODO Münzen auffüllen!", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private View.OnClickListener btnEndMaintenancePressed = new View.OnClickListener() {
+        public void onClick(View v) {
+
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.mainFragmentContainer, FragmentOverview)
+                    .addToBackStack(null)
+                    .commitAllowingStateLoss();
+        }
+    };
 }
