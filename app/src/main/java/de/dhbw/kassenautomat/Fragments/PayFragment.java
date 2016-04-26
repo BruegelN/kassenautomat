9112648b9dbf9129c36d609d24a407b44ffa4d6b
BruegelN <Nico.Bruegel@gmx.de>
2016-04-26 11:15:41 +0200

    PayFragment.
    Renamed layoutView to layoutPay.
    Create Buttons for navigation and ImageButtons for coins.
    Register nClickListener for all theses buttons.
    On „abort“ pressed return to OverviewFragment.
    On „Quittung“ pressed show TOAST with TODO, need to create bill.
    For each press on a coin images a TOAST with the correct value is
    shown. Need to reduce the money left to pay every time!
    Later a data structure to handle the amount of money to pay is needed!

--- app/src/main/java/de/dhbw/kassenautomat/Fragments/PayFragment.java
+++ app/src/main/java/de/dhbw/kassenautomat/Fragments/PayFragment.java
@@ -6,6 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 // To access the XML layouts easily
 import de.dhbw.kassenautomat.R;
@@ -15,10 +18,28 @@ import de.dhbw.kassenautomat.R;
  */
 public class PayFragment extends Fragment {
 
+    // TODO Need a data structure to save already payed money and calculate return money!
+
+    // Regular buttons
+    private Button btnQuittung;
+    private Button btnAbort;
+
+    // Buttons for coins
+    private ImageButton btnFiveCent;
+    private ImageButton btnTenCent;
+    private ImageButton btnTwentyCent;
+    private ImageButton btnFiftyCent;
+    private ImageButton btnOneEuro;
+    private ImageButton btnTwoEuro;
+
+    private OverviewFragment FragmentOverview;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         // TODO initialize fields before createView
         super.onCreate(savedInstanceState);
+
+        FragmentOverview = (OverviewFragment) Fragment.instantiate(this.getActivity(), OverviewFragment.class.getName(), null);
     }
 
     @Nullable
@@ -28,14 +49,110 @@ public class PayFragment extends Fragment {
         /**
          * Set the layout where you can pay a ticket with the given LayoutInflater.
          */
-        View layoutView = inflater.inflate(R.layout.fragment_pay, null);
+        View layoutPay = inflater.inflate(R.layout.fragment_pay, null);
+
+        /**
+         * Bind buttons to corresponding view element identified by their ID.
+         * And register button-click handlers.
+         */
+        btnQuittung = (Button) layoutPay.findViewById(R.id.btnQuittung);
+        btnAbort = (Button) layoutPay.findViewById(R.id.btnAbort);
+        btnQuittung.setOnClickListener(btnQuittungPressed);
+        btnAbort.setOnClickListener(btnAbortPressed);
+
+        /**
+         * Wire money-buttons to UI elements.
+         */
+        btnFiveCent = (ImageButton) layoutPay.findViewById(R.id.btnFiveCent);
+        btnTenCent = (ImageButton) layoutPay.findViewById(R.id.btnTenCent);
+        btnTwentyCent = (ImageButton) layoutPay.findViewById(R.id.btnTwentyCent);
+        btnFiftyCent = (ImageButton) layoutPay.findViewById(R.id.btnFiftyCent);
+        btnOneEuro = (ImageButton) layoutPay.findViewById(R.id.btnOneEuro);
+        btnTwoEuro = (ImageButton) layoutPay.findViewById(R.id.btnTwoEuro);
+
+        /**
+         * Register onClick handlers for money button.
+         * Those are called when someone "pays" a amount of money.
+         */
+        btnFiveCent.setOnClickListener(btnFiveCentPressed);
+        btnTenCent.setOnClickListener(btnTenCentPressed);
+        btnTwentyCent.setOnClickListener(btnTwentyCentPressed);
+        btnFiftyCent.setOnClickListener(btnFiftyCentPressed);
+        btnOneEuro.setOnClickListener(btnOneEuroPressed);
+        btnTwoEuro.setOnClickListener(btnTwoEuroPressed);
+
 
         // so it can be displayed
-        return layoutView;
+        return layoutPay;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
     }
+
+    /**
+     * onClickListener to get the bill after payed ticket!
+     *
+     */
+    private View.OnClickListener btnQuittungPressed = new View.OnClickListener() {
+        public void onClick(View v) {
+
+            // TODO check already enough payed!
+            Toast.makeText(getActivity(), "TODO Quittung", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    /**
+     * onClickListener to abort pay ticket process.
+     *
+     */
+    private View.OnClickListener btnAbortPressed = new View.OnClickListener() {
+        public void onClick(View v) {
+
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.mainFragmentContainer, FragmentOverview)
+                    .addToBackStack(null)
+                    .commitAllowingStateLoss();
+        }
+    };
+
+
+
+    private View.OnClickListener btnFiveCentPressed = new View.OnClickListener() {
+        public void onClick(View v) {
+            // TODO reduce amount of money to pay
+            Toast.makeText(getActivity(), "5 ct bezahlt", Toast.LENGTH_SHORT).show();
+        }
+    };
+    private View.OnClickListener btnTenCentPressed = new View.OnClickListener() {
+        public void onClick(View v) {
+            // TODO reduce amount of money to pay
+            Toast.makeText(getActivity(), "10 ct bezahlt", Toast.LENGTH_SHORT).show();        }
+    };
+    private View.OnClickListener btnTwentyCentPressed = new View.OnClickListener() {
+        public void onClick(View v) {
+            // TODO reduce amount of money to pay
+            Toast.makeText(getActivity(), "20 ct bezahlt", Toast.LENGTH_SHORT).show();
+        }
+    };
+    private View.OnClickListener btnFiftyCentPressed = new View.OnClickListener() {
+        public void onClick(View v) {
+            // TODO reduce amount of money to pay
+            Toast.makeText(getActivity(), "50 ct bezahlt", Toast.LENGTH_SHORT).show();
+        }
+    };
+    private View.OnClickListener btnOneEuroPressed = new View.OnClickListener() {
+        public void onClick(View v) {
+            // TODO reduce amount of money to pay
+            Toast.makeText(getActivity(), "1 € bezahlt", Toast.LENGTH_SHORT).show();
+        }
+    };
+    private View.OnClickListener btnTwoEuroPressed = new View.OnClickListener() {
+        public void onClick(View v) {
+            // TODO reduce amount of money to pay
+            Toast.makeText(getActivity(), "2 € bezahlt", Toast.LENGTH_SHORT).show();
+        }
+    };
+
 }
