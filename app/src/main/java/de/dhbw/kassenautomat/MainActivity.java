d17ec29f92b53ab71c64739842ff4419bf5b407d
it14078 <it14078@lehre.dhbw-stuttgart.de>
2016-04-28 11:44:13 +0200

    Added payment method to PayFragment
    + some necessary static things implemented into MainActivity

--- app/src/main/java/de/dhbw/kassenautomat/MainActivity.java
+++ app/src/main/java/de/dhbw/kassenautomat/MainActivity.java
@@ -2,17 +2,25 @@ package de.dhbw.kassenautomat;
 
 
 import android.app.Fragment;
+import android.content.Context;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import de.dhbw.kassenautomat.Database.DatabaseManager;
 import de.dhbw.kassenautomat.Fragments.OverviewFragment;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static MainActivity instance;
+    private static DatabaseManager DBmanager;
     private OverviewFragment FragmentOverview;
 
+    public MainActivity()
+    {
+        instance = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +40,16 @@ public class MainActivity extends AppCompatActivity {
                             .addToBackStack(null)
                             .commit();
 
+        DBmanager = new DatabaseManager(getContext());
+    }
+
+    public static Context getContext()
+    {
+        return instance.getApplicationContext();
+    }
+
+    public static DatabaseManager getDBmanager()
+    {
+        return DBmanager;
     }
 }
