7a5087d35ceb7fad76a74f26389b97f09f00df8f
BruegelN <Nico.Bruegel@gmx.de>
2016-05-03 10:44:32 +0200

    Added backPressedHandler in MainActivity.
    When ever the back button is pressed a TOAST is shown if the user
    presses the back button again within two seconds the app is closed
    otherwise nothin happens.

--- app/src/main/java/de/dhbw/kassenautomat/MainActivity.java
+++ app/src/main/java/de/dhbw/kassenautomat/MainActivity.java
@@ -2,8 +2,10 @@ package de.dhbw.kassenautomat;
 
 
 import android.app.Fragment;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import de.dhbw.kassenautomat.Fragments.OverviewFragment;
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private OverviewFragment FragmentOverview;
+    private boolean doubleBackToExitPressedOnce = false;
 
 
     @Override
@@ -32,5 +35,32 @@ public class MainActivity extends AppCompatActivity {
                             .addToBackStack(null)
                             .commit();
 
+    }
+
+    /**
+     * This global handler is called whenever the BACK-button is pressed.
+     * To close the app via back button you have to press it twice within two seconds.
+     * On the first press only a hint is given.
+     */
+    @Override
+    public void onBackPressed() {
+        //Checking for fragment count on backstack
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            getSupportFragmentManager().popBackStack();
+        } else if (!doubleBackToExitPressedOnce) {
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this,R.string.strPressBackAgainToExit, Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 2000);
+        } else {
+            super.onBackPressed();
+            return;
+        }
     }
 }
