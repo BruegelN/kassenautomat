45d573cc24f29ab520e1d33ad9cb10db75bf02b6
it14078 <it14078@lehre.dhbw-stuttgart.de>
2016-05-10 10:47:31 +0200

    Declaration of primary keys added to DB

--- app/src/main/java/de/dhbw/kassenautomat/Database/MySQLiteOpenHelper.java
+++ app/src/main/java/de/dhbw/kassenautomat/Database/MySQLiteOpenHelper.java
@@ -15,7 +15,7 @@ import de.dhbw.kassenautomat.COIN_DATA;
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
     Context c;
     public static final String DB_NAME = "kassenautomat.db";
-    public static final int VERSION = 3; //We "start" with DB version 1
+    public static final int VERSION = 5; //We "start" with DB version 1
 
     public MySQLiteOpenHelper(Context c)
     {
@@ -32,8 +32,8 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
     {
         // CREATE all necessary tables
         db.execSQL("CREATE TABLE tickets (id INTEGER PRIMARY KEY AUTOINCREMENT, created DateTime, print_quality int, paid bool)");
-        db.execSQL("CREATE TABLE receipt (FKid int, paid DateTime, price int)");
-        db.execSQL("CREATE TABLE coins (value int, level int)");
+        db.execSQL("CREATE TABLE receipt (FKid INTEGER PRIMARY KEY, paid DateTime, price int)");
+        db.execSQL("CREATE TABLE coins (value INTEGER PRIMARY KEY, level int)");
 
         // Insert all COINS into the coins table starting with MAX_COIN_LVL
         for (int coin: COIN_DATA.COINS) {
