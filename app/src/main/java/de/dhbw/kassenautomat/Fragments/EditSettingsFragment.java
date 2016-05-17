0f6ed19e424024cd7775e1aa642813caf8655634
it14078 <it14078@lehre.dhbw-stuttgart.de>
2016-05-17 17:21:10 +0200

    Implemented basic functions to edit settings

--- app/src/main/java/de/dhbw/kassenautomat/Fragments/EditSettingsFragment.java
+++ app/src/main/java/de/dhbw/kassenautomat/Fragments/EditSettingsFragment.java
@@ -2,10 +2,12 @@ package de.dhbw.kassenautomat.Fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.FloatRange;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,10 +25,16 @@ public class EditSettingsFragment extends Fragment{
     private TextView txt_PricePerHalfHour;
     private TextView txt_RejectedCoinsShare;
 
+    private Button btn_Save;
+    private Button btn_Back;
+
+    MaintenanceFragment FragmentMaintenance;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FragmentMaintenance = (MaintenanceFragment) Fragment.instantiate(this.getActivity(), MaintenanceFragment.class.getName(), null);
     }
 
     @Nullable
@@ -34,34 +42,70 @@ public class EditSettingsFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View layout_editSettings = inflater.inflate(R.layout.fragment_edit_settings, container, false);
+
         txt_maxCoinLevel = (TextView) layout_editSettings.findViewById(R.id.txt_maxCoinLevel);
         txt_defaultCoinLevel = (TextView) layout_editSettings.findViewById(R.id.txt_defaultCoinLevel);
         txt_PricePerHalfHour = (TextView) layout_editSettings.findViewById(R.id.txt_PricePerHalfHour);
         txt_RejectedCoinsShare = (TextView) layout_editSettings.findViewById(R.id.txt_RejectedCoinsShare);
 
 
+        btn_Save = (Button) layout_editSettings.findViewById(R.id.btn_SaveSettings);
+        btn_Back = (Button) layout_editSettings.findViewById(R.id.btn_Abort);
+
+        btn_Save.setOnClickListener(btnSavePressed);
+        btn_Back.setOnClickListener(btnBackPressed);
+
         updateValues();
 
         return  layout_editSettings;
     }
 
+    private View.OnClickListener btnBackPressed = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            goBackToMaintenance();
+        }
+    };
+
+    private View.OnClickListener btnSavePressed = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            saveSettings();
+            goBackToMaintenance();
+        }
+    };
+
     private void saveSettings()
     {
-        //TODO use values from txt fields
-        String message = COIN_DATA.setSettings(MainActivity.getDBmanager(), 300, 0.10f, 0.60f, 0.30f);
+        int maxCoinLevel = Integer.parseInt(txt_maxCoinLevel.getText().toString());
+        float defaultCoinLevel = Integer.parseInt(txt_defaultCoinLevel.getText().toString())/(float)100;
+        float pricePerHalfHour = Float.parseFloat(txt_PricePerHalfHour.getText().toString());
+        float rejectedCoinsShare = Integer.parseInt(txt_RejectedCoinsShare.getText().toString())/(float)100;
+
+        String message = COIN_DATA.setSettings(MainActivity.getDBmanager(), maxCoinLevel, defaultCoinLevel, pricePerHalfHour, rejectedCoinsShare);
 
         if (message == "")
             message = "Einstellungen gespeichert";
 
-        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
 
         COIN_DATA.readConfig(MainActivity.getDBmanager());
+
+        goBackToMaintenance();
     }
 
     private void updateValues() {
         txt_maxCoinLevel.setText(Integer.toString(COIN_DATA.MAX_COIN_LVL));
         txt_defaultCoinLevel.setText(Integer.toString((int)(COIN_DATA.DEFAULT_COIN_LEVEL*100)));
-        txt_PricePerHalfHour.setText(String.format("%,.2f",COIN_DATA.COST_PER_HALF_HOUR / (float) 100));
-        txt_RejectedCoinsShare.setText(Integer.toString((int)(COIN_DATA.REJECTED_COINS_SHARE*100)));
+        txt_PricePerHalfHour.setText(Float.toString(COIN_DATA.COST_PER_HALF_HOUR / (float) 100));
+        txt_RejectedCoinsShare.setText(Integer.toString((int) (COIN_DATA.REJECTED_COINS_SHARE * 100)));
+    }
+
+    private void goBackToMaintenance()
+    {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.mainFragmentContainer, FragmentMaintenance)
+                .addToBackStack(null)
+                .commitAllowingStateLoss();
     }
 }
